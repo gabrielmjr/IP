e1469a00add73fd8705d5a0d541e08e62b5e69f5
Gabriel MJr <gmjplayys@gmail.com>
2024-11-14 14:39:26 +0200

    refactor: Refactored lesson8/Exercise3.java

--- src/main/java/com/mjr/code/ip/lesson8/Exercise3.java
+++ src/main/java/com/mjr/code/ip/lesson8/Exercise3.java
@@ -3,28 +3,22 @@ package com.mjr.code.ip.lesson8;
 import java.io.*;
 
 public class Exercise3 {
-    private static final int NOTA = 1;
-
-    private static BufferedReader bReader;
-
     public static void main(String[] args) throws IOException {
-        bReader = new BufferedReader(new InputStreamReader(System.in));
-        File file = criarFicheiro();
-        String[] infoDeEstudantes = lerInfoDeEstudantes();
-        escreverNoFicheiro(infoDeEstudantes, file);
-        lerDoFicheiro(file);
+        criarFicheiro();
+        escreverNoFicheiro();
+        lerDoFicheiro();
     }
 
-    private static File criarFicheiro() throws IOException {
-        System.out.print("Nome do ficheiro: ");
-        String nome = bReader.readLine();
-        File file = new File(nome);
+    private static void criarFicheiro() throws IOException {
+        File file = new File("exercise3.txt");
         if (!file.exists())
             file.createNewFile();
-        return file;
     }
 
-    private static String[] lerInfoDeEstudantes() throws IOException {
+    private static void escreverNoFicheiro() throws IOException {
+        BufferedReader bReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bWriter = new BufferedWriter(new FileWriter("exercise3.txt"));
+
         int quantidadeDeEstudantes;
         do {
             System.out.print("Número de estudantes: ");
@@ -33,11 +27,9 @@ public class Exercise3 {
                 System.out.println("Quantidade de estudantes inválida");
         } while (quantidadeDeEstudantes < 1);
 
-        String[] infoDeEstudantes = new String[quantidadeDeEstudantes];
         for (int i = 0; i < quantidadeDeEstudantes; i++) {
             System.out.print("Nome do estudante " + (i + 1) + ": ");
-            infoDeEstudantes[i] = bReader.readLine();
-            infoDeEstudantes[i] += " : ";
+            bWriter.write(bReader.readLine());
             float notaDoEstudante;
             do {
                 System.out.print("Nota do estudante: ");
@@ -45,32 +37,24 @@ public class Exercise3 {
                 if (notaDoEstudante < 0 || notaDoEstudante > 20)
                     System.out.println("Nota inválida, tente novamente.");
             } while (notaDoEstudante < 0 || notaDoEstudante > 20);
-            infoDeEstudantes[i] += notaDoEstudante;
+
+            bWriter.write(" : " + notaDoEstudante);
+            bWriter.newLine();
             System.out.println();
         }
 
-        return infoDeEstudantes;
-    }
-
-    private static void escreverNoFicheiro(String[] infoDeEstudantes, File file) throws IOException {
-        FileWriter fileWriter = new FileWriter(file);
-        BufferedWriter bWriter = new BufferedWriter(fileWriter);
-        for (String infoDeEstudante : infoDeEstudantes)
-            bWriter.append(infoDeEstudante).append(System.lineSeparator());
         bWriter.close();
-        fileWriter.close();
     }
 
-    private static void lerDoFicheiro(File file) throws IOException {
-        FileReader fileReader = new FileReader(file);
-        bReader = new BufferedReader(fileReader);
+    private static void lerDoFicheiro() throws IOException {
+        BufferedReader bReader = new BufferedReader(new FileReader("exercise3.txt"));
         String line;
         int numeroDeEstudantes = 0;
         System.out.println("Os estudantes com nota superior a 13 são: ");
         while ((line = bReader.readLine()) != null) {
             numeroDeEstudantes++;
             String[] infoDeEstudante = line.split(" : ");
-            if (Float.parseFloat(infoDeEstudante[NOTA]) > 13) {
+            if (Float.parseFloat(infoDeEstudante[1]) > 13) {
                 System.out.println(line);
             }
         }
